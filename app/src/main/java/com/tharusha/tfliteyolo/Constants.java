edf30ae8e237e38bdd130087bd8c3f70406a4c36
Yoone <tinhanhlamaimai1991@gmail.com>
2025-09-25 15:30:13 +0700

    add yolo model

--- app/src/main/java/com/tharusha/tfliteyolo/Constants.java
+++ app/src/main/java/com/tharusha/tfliteyolo/Constants.java
@@ -1,6 +1,7 @@
 package com.tharusha.tfliteyolo;
 
 public class Constants {
-    public static final String MODEL_PATH = "yolo11s_f32.tflite";
+    //public static final String MODEL_PATH = "yolo11s_f32.tflite";
+    public static final String MODEL_PATH = "best_float32.tflite";
     public static final String CLASSES_PATH = "classes.txt";
 }
